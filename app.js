fdc41deffca2b665a428de1ad54160d0c66c7ca6
GloriaRamosM <gloriaramosmoran@gmail.com>
2023-11-20 15:41:03 -0300

    practica de ejs

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const app = express();
 const ejs = require("ejs");
+const expressLayouts = require("express-ejs-layouts");
+const methodOverride = require("method-override");
+
+const fs = require("fs");
+
 const PORT = 4000;
 
+//Para que lea mis archivos public
 app.use(express.static("public"));
 
 // Configuracion ejs
 app.set("view engine", "ejs");
 app.set("views", "./src/views");
 
+// CONFIGURACION LAYOUTS - y poner donde se encuentra posicionada( ya sabe que esta en vista porque arriba en ejs se lo dijimos)
+app.use(expressLayouts);
+app.set("layout", "layouts/layout");
+
+// METODO OVERRIDE
+app.use(express.urlencoded({ extend: false }));
+app.use(methodOverride("_method"));
+
+// RUTAS
 app.use(require("./src/routes/routes"));
 
 app.listen(PORT, () => {
